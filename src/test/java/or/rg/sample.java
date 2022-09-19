5e2ade1b1554d08671632c1652b35b865133a23e
latharaja2 <lathachandran865@gmail.com>
2022-09-19 17:55:50 +0530

    1st

---
+++ src/test/java/or/rg/sample.java
@@ -0,0 +1,22 @@
+package or.rg;
+
+public class sample {
+	public class GitClass {
+		public void method1() {
+			System.out.println("1");
+		}
+		public void method2() {
+			System.out.println("2"); 
+
+		}
+		public void method3() {
+			System.out.println("3"); 
+
+		}
+
+		
+
+	}
+
+
+}
